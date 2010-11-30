9768f782ebeb81e4e09ecc02a5ea5f920864ab58
Andrew Bruce <andrew@camelpunch.com>
2010-11-30 12:49:40 +0000

    durr... looping over File.open forever isn't so good for your system

--- test_server.rb
+++ test_server.rb
@@ -3,8 +3,8 @@
 pipe_path = File.join(ENV['HOME'], 'test_server_pipe')
 `mkfifo #{pipe_path}` unless File.exists?(pipe_path)
 
-while true do
-  File.open pipe_path, 'w+' do |file|
+File.open pipe_path, 'w+' do |file|
+  while true do
     command = file.gets
     system "clear"
     puts command
